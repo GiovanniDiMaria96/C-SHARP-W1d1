1f86803abae695179b9ac41ade4b6d9282ef9dff
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-23 10:34:43 +0200

    Add files via upload

---
+++ ConsoleApp6/ConsoleApp6/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Action NoParametri = () => { Console.WriteLine("Ciao Mondo"); };
+            NoParametri();
+
+            Action<string> Func = (nome) => { Console.WriteLine("Benvenuto " + nome); };
+            Func("Giovanni");
+
+            Action<int, int> Somma = (a, b) => { Console.WriteLine(a + b); };
+            Somma(2, 6);
+
+            Action<List<string>> Iterazione = (lista) =>
+            {
+
+                Console.WriteLine(string.Join(",", lista).ToUpper());
+            };
+
+            var lista = new List<string> { "giovanni", "francesco", "giuseppe" };
+            Iterazione(lista);
+
+
+            Action<string> stampa = messaggio => Console.WriteLine(messaggio);
+            EseguiOperazioneSicura("...Lavoro in corso...", stampa);
+        }
+
+        public static void EseguiOperazioneSicura(string x, Action<string> y)
+        {
+
+            Console.WriteLine("Inizio operazione");
+
+            try
+            {
+                y(x);
+                Console.WriteLine("Fine operazione");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Errore: {ex.Message}");
+                throw;
+            }
+
+        }
+
+
+    }
+}
