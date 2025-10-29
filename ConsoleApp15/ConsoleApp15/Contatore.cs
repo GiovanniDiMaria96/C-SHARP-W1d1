2b674a7f3337809d92ec837a41fb980aeaa4d25e
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-29 12:11:22 +0100

    Add files via upload

---
+++ ConsoleApp15/ConsoleApp15/Contatore.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    
+    internal class Contatore
+    {
+        public int conteggio = 0;
+        
+        public int Incrementa()
+        {
+            for (int i = 0; i < 100000; i++)
+            {
+                conteggio++;
+            }
+            return conteggio;
+
+        }
+    }
+}
---
+++ ConsoleApp15/ConsoleApp15/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+
+namespace ConsoleApp15
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Contatore contatore = new Contatore();
+
+            Task task1 = Task.Run(() => contatore.Incrementa());
+            Task task2 = Task.Run(() => contatore.Incrementa()); //QUESTO DIMOSTRA COME DUE TASK NON FUNZIONANO COME DOVREBBERO
+                                                                 // PER ESEMPIO IN UN CONTEGGIO
+            await Task.WhenAll(task1, task2);
+            Console.WriteLine(contatore.conteggio);
+        }
+    }
+}
