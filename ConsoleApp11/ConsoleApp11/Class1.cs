1b3ccf361325f58b33865e07f9f7f14d3db80b4b
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-29 10:13:22 +0100

    Add files via upload

---
+++ ConsoleApp11/ConsoleApp11/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp11
+{
+    internal class Class1
+    {
+
+        public void Stampa()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(200);
+            }
+        }
+
+    }
+}
---
+++ ConsoleApp11/ConsoleApp11/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            Task t = Task.Run(() => class1.Stampa());
+            Console.WriteLine("Thread principale in esecuzione");
+       
+            t.Wait();
+            Console.WriteLine("Thread secondario terminato");
+        }
+    }
+}
