1c0eff8885160fccdd035c3f5af9b54638ce9014
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-29 16:58:44 +0100

    Add files via upload

---
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -0,0 +1,30 @@
+﻿    using System.Text;
+
+    namespace ConsoleApp17
+    {
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                string path = @"C:\Users\Laboratorio12\Desktop\Esercizi_C#\saluto.txt";
+
+                try
+                {
+
+                    File.WriteAllText(path, "Ciao mondo!");
+                    Console.WriteLine("saluto.txt creato");
+                    
+
+                }
+
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.ToString());
+
+                }
+            
+            }
+
+        }
+    }
+
