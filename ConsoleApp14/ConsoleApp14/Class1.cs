1f5f15c7377a381267bab2f037499d0afc721bc7
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-29 11:34:23 +0100

    Add files via upload

---
+++ ConsoleApp14/ConsoleApp14/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Class1
+    {
+        public async Task DownloadFile1Async()
+        {
+            Console.WriteLine("Inizio download 2 sec.");
+            await Task.Delay(2000);
+            Console.WriteLine("Fine download 2 sec.");
+        }
+    
+        public async Task DownloadFile2Async()
+        {
+            Console.WriteLine("Inizio download 3 sec.");
+            await Task.Delay(3000);
+            Console.WriteLine("Fine download 3 sec.");
+
+        }
+
+        public async Task Esegui()
+        {
+            Task t1 = DownloadFile1Async();
+            Task t2 = DownloadFile2Async();
+
+            await Task.WhenAll(t1, t2);
+        }
+
+    }
+}
---
+++ ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+
+namespace ConsoleApp14
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            await Task.Run(() => class1.Esegui());
+        }
+    }
+}
