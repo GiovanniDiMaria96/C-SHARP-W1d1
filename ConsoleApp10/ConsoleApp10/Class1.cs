052edc61dd3d14818ba4d8a41543aecda3ec9dcf
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-29 09:53:01 +0100

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    public class Class1
+    {
+         public void Stampa()
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp10
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            Thread t = new Thread(class1.Stampa);
+            t.Start();
+            Console.WriteLine("Thread principale in esecuzione");
+            t.Join();
+            Console.WriteLine("Thread secondario terminato.");
+        }
+    }
+}
