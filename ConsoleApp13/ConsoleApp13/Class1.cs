093eaa9a77e990d14f18e010e645e621f01ba9cc
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-29 11:11:31 +0100

    Add files via upload

---
+++ ConsoleApp13/ConsoleApp13/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    public class Class1
+    {
+       public async Task<string>  SimulaDownloadAsync()
+        {
+            Console.WriteLine("Download iniziato...");
+            await Task.Delay(2000);
+            return "Download completato";
+
+
+        }
+    }
+}
---
+++ ConsoleApp13/ConsoleApp13/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+
+namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+            Console.WriteLine("Avvia download");
+            // await Task.Run(() => class1.SimulaDownloadAsync());
+            var x = await class1.SimulaDownloadAsync();
+            Console.WriteLine(x);
+        }
+    }
+}
