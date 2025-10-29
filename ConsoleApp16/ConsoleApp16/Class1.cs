426a084eecb789f5b8bef61d1de4ec06a6444a6c
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-29 13:03:10 +0100

    Add files via upload

---
+++ ConsoleApp16/ConsoleApp16/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    public class Class1
+    {
+        public async Task BottonePremuto_Click()
+        {
+            Console.WriteLine("Operazione avviata...");
+            var x = await OperazioneLungaAsync();
+            Console.WriteLine( x.ToString() );
+
+
+        }
+
+        public async Task<string> OperazioneLungaAsync()
+        {
+            await Task.Delay(3000);
+            var x = "Risultato pronto";
+            return x;
+        }
+    }
+}
---
+++ ConsoleApp16/ConsoleApp16/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+
+namespace ConsoleApp16
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var class1 = new Class1();
+            var task = class1.BottonePremuto_Click();
+
+
+
+            while (!task.IsCompleted)
+            {   
+                Console.WriteLine(".");
+                await Task.Delay(500);
+            }
+        }
+    }
+}
