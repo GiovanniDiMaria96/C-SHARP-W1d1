cb851a5db3a509f200a5af8b938d721c1f22d630
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-29 10:32:26 +0100

    Add files via upload

---
+++ ConsoleApp12/ConsoleApp12/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    internal class Class1
+    {
+        public void Stampa()
+        {
+            Console.WriteLine(42);
+            Thread.Sleep(2000);
+        }
+    }
+}
---
+++ ConsoleApp12/ConsoleApp12/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            Task t = Task.Run(() => class1.Stampa());
+            Console.WriteLine("Inizio");
+            t.Wait();
+            Console.WriteLine("Fine");
+
+        }
+    }
+}
