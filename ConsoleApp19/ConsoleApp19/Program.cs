7ecafe56a35f90c37f3fc4d4863d8d8e73e6ce1e
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-30 15:40:32 +0100

    Update Program.cs

--- ConsoleApp19/ConsoleApp19/Program.cs
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -1,4 +1,4 @@
-﻿using System.Text.Json;
+using System.Text.Json;
 
 namespace ConsoleApp19
 {
@@ -9,12 +9,27 @@ namespace ConsoleApp19
             var utente = new Utente { Nome = "Giovanni", Eta = 29 };
 
             string js = JsonSerializer.Serialize(utente);
-            Console.WriteLine(js);
+            Console.WriteLine("Questo è il serializer " + js);
             string utjs = @"C:\Users\Laboratorio12\Desktop\Esercizi_C#\utente.json";
 
             try
             {
                 File.WriteAllText(utjs, js);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            try
+            {
+                var text = File.ReadAllText(utjs);
+                Utente des = JsonSerializer.Deserialize<Utente>(text);
+                string nome = des.Nome;
+                int eta = des.Eta;
+                //var utente1 = new Utente { Nome=nome, Eta=eta };
+                Console.WriteLine($"Nome: {nome} Età: {eta}");
+
             }
             catch (Exception ex)
             {
