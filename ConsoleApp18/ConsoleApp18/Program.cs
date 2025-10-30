b4277b86d6cf946e1b05985d0172082bab99859f
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-30 11:36:54 +0100

    Add files via upload

---
+++ ConsoleApp18/ConsoleApp18/Program.cs
@@ -0,0 +1,49 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace ConsoleApp18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //string log = @"C:\Users\Laboratorio12\Desktop\Esercizi_C#\log.txt";
+            //try
+            //{
+            //    string write = "Questa è il testo.  ";
+            //    File.WriteAllText(log, write);
+
+            //    if (File.Exists(log))
+            //    {
+
+            //        string append = "Questo è il testo aggiuntivo";
+            //        File.AppendAllText(log, append);
+            //        Console.WriteLine("Applicazione avviata");
+            //    }
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.ToString());
+            //}
+
+
+
+            //try
+            //{
+            //string testoavanzato = @"C:\Users\Laboratorio12\Desktop\Esercizi_C#\testoavanzato.txt";
+            //    string a1 = "Questa è una stringa";
+            //    using (Stream stream = new FileStream(testoavanzato, FileMode.Create, FileAccess.Write))
+            //    using (StreamWriter wr = new StreamWriter(stream))
+            //    {
+            //        wr.WriteLine("Prima riga");
+            //        wr.WriteLine("Seconda riga");
+            //        wr.WriteLine("Terza riga");
+            //    }
+
+            //} catch (Exception ex)
+            //{
+            //    Console.WriteLine(ex.ToString);
+            //}
+        }
+    }
+}
