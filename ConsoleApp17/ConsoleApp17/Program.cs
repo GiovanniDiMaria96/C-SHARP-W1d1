c7038da94bc3a83d8551969028c7c45c908cfe78
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-30 09:17:23 +0100

    Update Program.cs

--- ConsoleApp17/ConsoleApp17/Program.cs
+++ ConsoleApp17/ConsoleApp17/Program.cs
@@ -1,30 +1,42 @@
-﻿    using System.Text;
+using System.Text;
 
-    namespace ConsoleApp17
+namespace ConsoleApp17
+{
+    internal class Program
     {
-        internal class Program
+        static void Main(string[] args)
         {
-            static void Main(string[] args)
+            string path = @"C:\Users\Laboratorio12\Desktop\Esercizi_C#\saluto.txt";
+
+            try
             {
-                string path = @"C:\Users\Laboratorio12\Desktop\Esercizi_C#\saluto.txt";
 
-                try
-                {
+                File.WriteAllText(path, "Ciao mondo!");
+                Console.WriteLine("saluto.txt creato");
 
-                    File.WriteAllText(path, "Ciao mondo!");
-                    Console.WriteLine("saluto.txt creato");
-                    
-
-                }
-
-                catch (Exception ex)
-                {
-                    Console.WriteLine(ex.ToString());
-
-                }
-            
             }
 
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+
+            try
+            {
+                var text = File.ReadAllText(path);
+                Console.WriteLine(text);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+
+
         }
+
     }
+}
 
