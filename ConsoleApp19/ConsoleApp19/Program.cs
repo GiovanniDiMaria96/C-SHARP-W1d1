233a8630a4f1946e6c8282b37b6ca73b21adfbff
GiovanniDiMaria <158842385+GiovanniDiMaria96@users.noreply.github.com>
2025-10-30 12:38:06 +0100

    Add files via upload

---
+++ ConsoleApp19/ConsoleApp19/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace ConsoleApp19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var utente = new Utente { Nome = "Giovanni", Eta = 29 };
+
+            string js = JsonSerializer.Serialize(utente);
+            Console.WriteLine(js);
+            string utjs = @"C:\Users\Laboratorio12\Desktop\Esercizi_C#\utente.json";
+
+            try
+            {
+                File.WriteAllText(utjs, js);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ ConsoleApp19/ConsoleApp19/Utente.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    public class Utente
+    {
+        public string Nome {  get; set; }
+        public int Eta {  get; set; }
+    }
+}
